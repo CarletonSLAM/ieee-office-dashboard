2d4723294f0b035e7dad20d85a2b0d8dea44d4bd
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2019-04-07 19:58:14 -0400

    Added social media connector functionality to return credentials

--- django/dashyserver/api/urls.py
+++ django/dashyserver/api/urls.py
@@ -1,13 +1,9 @@
 from django.urls import include, path
-from rest_framework import routers
 from api import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('credentials/<str:type>/', views.CredentialDetail.as_view()),
 ]

90be5cde17423c3aca6873d4dcb4f5d40b56fb4b
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2019-05-12 08:36:29 -0400

    Add stops initial work

--- django/api/views.py
+++ django/api/views.py
@@ -33,3 +33,23 @@ class CredentialDetail(APIView):
             else:
                 return JsonResponse(status=404, data={'detail':'User not registered with provider'})
         return JsonResponse(data=context, status=200)
+
+
+OCTRANSPO_GET_BASE_URL='https://api.octranspo1.com/v1.2/GetNextTripsForStopAllRoutes?format=json&appID={}&apiKey={}&stopNo={}'
+
+class ServiceRequest(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, type):
+        if type is not 'octranspo':
+            return JsonResponse(status=400, data={'detail':'Unknown provider'})
+
+        provider = APIKeyProviderSerializer(APIKeyProvider.objects.filter(name=type).first()).data
+        stop_nums = request.GET['stops']
+        res = requests.get(OCTRANSPO_GET_BASE_URL.format(provider['api_id'],provider['api_key'], stop_nums))
+
+        context = res.json()
+        if res.status_code is not 200:
+            return JsonResponse(status=res.status_code, data={'detail': res.text})
+
+        return JsonResponse(status=200, data=context)

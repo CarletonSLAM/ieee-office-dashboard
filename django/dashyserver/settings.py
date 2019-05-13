12d02b5fcf3a43f692d01a4612c936273e682351
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2019-05-12 23:45:36 -0400

    Narrow scope for reading picture metadata + remove GAPI client

--- django/dashyserver/settings.py
+++ django/dashyserver/settings.py
@@ -180,7 +180,7 @@ SOCIALACCOUNT_PROVIDERS = {
         'SCOPE': [
             'https://www.googleapis.com/auth/userinfo.profile',
             'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/drive.readonly',
+            'https://www.googleapis.com/auth/drive.metadata.readonly',
             'https://www.googleapis.com/auth/calendar.events.readonly',
         ],
         'AUTH_PARAMS': {

55080dc881b4501643bebfc2802627d4281ac230
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2019-05-13 22:16:35 -0400

    Remove Express server and RSA Token generator for old auth mechanism

--- server/api/facebook.js
+++
@@ -1,9 +0,0 @@
-const { facebook } = JSON.parse(Buffer.from(require('../config'), 'base64').toString('binary'))
-
-module.exports = {
-    path: '/facebook',
-    digester: async (ctx, next) => {
-        ctx.body = { success: true, data: Buffer.from(JSON.stringify(facebook, 'binary')).toString('base64') }
-        await next()
-    }
-}
--- server/api/google.js
+++
@@ -1,9 +0,0 @@
-const { google } = JSON.parse(Buffer.from(require('../config'), 'base64').toString('binary'))
-
-module.exports = {
-    path: '/google',
-    digester: async (ctx, next) => {
-        ctx.body = { success: true, data: Buffer.from(JSON.stringify(google, 'binary')).toString('base64') }
-        await next()
-    }
-}

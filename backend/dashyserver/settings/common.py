da348ca181e61827a07c38f61768015ce5a7ed70
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2019-05-28 06:36:55 -0400

    Synched dev with heroku files

--- backend/dashyserver/settings/common.py
+++ backend/dashyserver/settings/common.py
@@ -75,42 +75,6 @@ CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'dashyserver.urls'
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-BACKEND_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
-FRONTEND_DIR = os.path.abspath(os.path.join(BACKEND_DIR, '..', 'frontend'))
-
-STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build', 'static')]
-
-STATICFILES_STORAGE = (
-    'whitenoise.storage.CompressedManifestStaticFilesStorage')
-
-STATIC_ROOT = os.path.join(BACKEND_DIR, 'static')
-
-STATIC_URL = '/static/'  # already declared in the default settings
-
-WHITENOISE_ROOT = os.path.join(FRONTEND_DIR, 'build')
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BACKEND_DIR, 'pages/templates'),
-            os.path.join(FRONTEND_DIR, 'build')
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
 LOGIN_REDIRECT_URL = 'socialaccount_connections'
 LOGOUT_REDIRECT_URL = 'home'
 
--- backend/dashyserver/settings/prod.py
+++ backend/dashyserver/settings/prod.py
@@ -11,5 +11,36 @@ DEBUG = False
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+BACKEND_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
+STATICFILES_DIRS = [
+    os.path.join(BACKEND_DIR, 'frontend', 'static'),
+    os.path.join(BACKEND_DIR, 'pages', 'static'),
+]
+STATICFILES_STORAGE = (
+    'whitenoise.storage.CompressedManifestStaticFilesStorage')
+STATIC_ROOT = os.path.join(BACKEND_DIR, 'static')
+STATIC_URL = '/static/'  # already declared in the default settings
+WHITENOISE_ROOT = os.path.join(BACKEND_DIR, 'frontend')
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BACKEND_DIR, 'pages/templates'),
+            os.path.join(BACKEND_DIR, 'frontend')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())

b3cc6162f4e4ebed51fa3409a1b75bfe3d09351d
Irusha Vidanamadura <irushavm@users.noreply.github.com>
2019-06-07 07:36:22 -0400

    Update install.sh

--- backend/scripts/install.sh
+++ backend/scripts/install.sh
@@ -7,7 +7,7 @@ NC='\033[0m' # No Color
 echo -e "${YELLOW}In directory $PWD${NC}..."
 
 sudo apt-get update
-sudo apt-get install libssl-dev make build-essential libssl-dev zlib1g-dev libbz2-dev libsqlite3-dev libpq-dev postgresql-server-dev-all
+sudo apt-get install libssl-dev make build-essential libssl-dev zlib1g-dev libbz2-dev libsqlite3-dev libpq-dev postgresql-server-dev-all libffi-dev
 # Install pyenv if does not exist via https://github.com/pyenv/pyenv-installer
 
 if ! type "pyenv" &> /dev/null; then

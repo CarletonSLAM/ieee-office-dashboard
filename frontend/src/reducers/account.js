193c7966b57f7c17590e138854f5c39719372c87
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2019-06-26 22:51:28 -0400

    Add another redux action to track login refresh calls

--- frontend/src/reducers/account.js
+++ frontend/src/reducers/account.js
@@ -2,6 +2,7 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAILURE,
     CLEAR_LOGIN_MESSAGE,
+    LOGIN_REFRESH_BEGIN,
     LOGIN_REFRESH_SUCCESS
 } from '../actions'
 
@@ -23,10 +24,16 @@ const loginStateReducer = (state = { data: {}, error: {}, success: false }, acti
     case CLEAR_LOGIN_MESSAGE:
         delete state.error
         return Object.assign({}, state)
-    case LOGIN_REFRESH_SUCCESS:
+    case LOGIN_REFRESH_BEGIN:
         delete state.error
         return Object.assign({}, state, {
-            success: true,
+            beginRefresh: true,
+            data: Object.assign({}, state.data, {access: action.access})
+        })
+    case LOGIN_REFRESH_SUCCESS:
+        delete state.error
+        delete state.beginRefresh
+        return Object.assign({}, state, {
             data: Object.assign({}, state.data, {access: action.access})
         })
 
